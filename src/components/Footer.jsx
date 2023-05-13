6e3194fa07a46f6fb93989c6a9187e2902a4c1fa
abdulwafa31094 <abdulwafa31094@gmail.com>
2023-05-14 01:08:16 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container-fluid  d-flex justify-content-center align-items-center bg-dark text-white p-3'>
+        <h6>Developed by <b>WAFA</b></h6> 
+    </div>
+  )
+}
+
+export default Footer
